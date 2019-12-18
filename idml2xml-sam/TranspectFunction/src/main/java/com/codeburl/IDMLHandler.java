83c0bcb6dad4c7e14964b1de28d30fbe72e3af31
Keith Fahlgren <keith@codeburl.com>
2019-12-18 10:10:55 -0800

    Project skeleton for IDML to Hub XML conversion with Transpect (and XML Calabash) on AWS Lambda using SAM

---
+++ idml2xml-sam/TranspectFunction/src/main/java/com/codeburl/IDMLHandler.java
@@ -0,0 +1,7 @@
+package com.hederis;
+
+public class IDMLHandler {
+  public void handleEvent(String s) {
+    System.out.println("Hello, " + s);
+  }
+}
---
+++ idml2xml-sam/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sam build
