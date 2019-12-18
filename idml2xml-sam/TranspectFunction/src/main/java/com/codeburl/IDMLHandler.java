7cec40ab631ee8aa7ba385bee610787793aad624
Keith Fahlgren <keith@codeburl.com>
2019-12-18 11:13:49 -0800

    Begin invoking Main driver for XML Calabash

--- idml2xml-sam/TranspectFunction/src/main/java/com/codeburl/IDMLHandler.java
+++ idml2xml-sam/TranspectFunction/src/main/java/com/codeburl/IDMLHandler.java
@@ -1,7 +1,32 @@
-package com.hederis;
+package com.codeburl;
+
+import java.io.IOException;
+import com.xmlcalabash.drivers.Main;
 
 public class IDMLHandler {
-  public void handleEvent(String s) {
-    System.out.println("Hello, " + s);
+  public void handleEvent(S3Put event) throws IOException {
+    final String key = event.Records.get(0).s3.object.key;
+    final String[] args = new String[1];
+		args[0] = "";
+
+    System.out.println("S3 PUT happened! Key was: " + key);
+    Main calabash = new Main();
+    calabash.run(args);
   }
+
+	public static class S3Put {
+		public java.util.List<S3PutRecord> Records;
+
+		public static class S3PutRecord {
+			public S3PutRecordS3 s3;
+
+			public static class S3PutRecordS3 {
+				public S3PutRecordS3Object object;
+
+				public static class S3PutRecordS3Object {
+					public String key;
+				}
+			}
+		}
+	}
 }
