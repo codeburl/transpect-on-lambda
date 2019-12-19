9de8ec1e254cb578a9ba29033e499464148a2827
Keith Fahlgren <keith@codeburl.com>
2019-12-19 10:34:22 -0800

    Add Calabash and Transpect dependencies to build process

--- idml2xml-sam/TranspectFunction/src/main/java/com/codeburl/IDMLHandler.java
+++ idml2xml-sam/TranspectFunction/src/main/java/com/codeburl/IDMLHandler.java
@@ -7,7 +7,7 @@ public class IDMLHandler {
   public void handleEvent(S3Put event) throws IOException {
     final String key = event.Records.get(0).s3.object.key;
     final String[] args = new String[1];
-		args[0] = "";
+		args[0] = "xpl/idml2xml-frontend.xpl";
 
     System.out.println("S3 PUT happened! Key was: " + key);
     Main calabash = new Main();
--- idml2xml-sam/build.sh
+++ idml2xml-sam/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+if [ ! -d TranspectFunction/src/resources/idml2xml-frontend ]; then
+  echo "Downloading Transpect dependencies..."
+  echo
+  git clone --recursive https://github.com/transpect/idml2xml-frontend TranspectFunction/src/resources/idml2xml-frontend
+  echo
+fi
+
 sam build
