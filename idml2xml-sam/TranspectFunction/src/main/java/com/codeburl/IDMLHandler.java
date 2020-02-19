9062ad58468196346566b80304e2444e21f46654
Keith Fahlgren <keith@codeburl.com>
2020-02-19 10:02:17 -0800

    Keep the same basename in the output XML

--- idml2xml-sam/TranspectFunction/src/main/java/com/codeburl/IDMLHandler.java
+++ idml2xml-sam/TranspectFunction/src/main/java/com/codeburl/IDMLHandler.java
@@ -9,8 +9,10 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.xmlcalabash.drivers.Main;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -31,27 +33,27 @@ public class IDMLHandler {
 
     logger.info("Handling S3 PUT to " + input_bucket + "/" + input_key);
 
-		final String input_fn = "/tmp/input.idml";
-    downloadInputIDML(input_bucket, input_key, input_fn);
-    final String output_fn = invokeXProc(input_fn);
+    final String source_idml_fn = downloadInputIDML(input_bucket, input_key);
+    final String output_fn = invokeXProc(source_idml_fn);
     logger.info("HUB XML output sent to " + output_fn);
     final String output_bucket= System.getenv("OUTPUT_BUCKET");
-    final String result_arn = uploadOutputXML(output_bucket, output_fn);
+    final String result_arn = uploadOutputXML(output_bucket, output_fn, input_key);
     return result_arn;
   }
 
-  private String invokeXProc(String input_fn) throws IOException {
+  private String invokeXProc(String source_idml_fn) throws IOException {
     final String config_fn = "/var/task/transpect-config.xml";
     final String xpl_fn = "/var/task/xpl/idml2xml-frontend.xpl";
     //final String config_fn = this.getClass().getClassLoader().getResource("transpect-config.xml").toString();
     //final String xpl_fn = this.getClass().getClassLoader().getResource("xpl/idml2xml-frontend.xpl").toString();
     logger.info("Invoking XProc pipline (" + xpl_fn + ") with config " + config_fn);
-    String output_fn = "/tmp/output.xml";
+    UUID uuid = UUID.randomUUID();
+    final String output_fn = "/tmp/" + uuid.toString() + ".xml";
 		final String[] args = new String[5];
 		args[0] = "--config=" + config_fn;
 		args[1] = "-oresult=" + output_fn;
 		args[2] = xpl_fn;
-		args[3] = "idml=" + input_fn;
+		args[3] = "idml=" + source_idml_fn;
 		args[4] = "status-dir-uri=/tmp";
 
     Main calabash = new Main();
@@ -59,12 +61,14 @@ public class IDMLHandler {
     return output_fn;
   }
 
-  private void downloadInputIDML(String bucket, String key, String input_fn) {
+  private String downloadInputIDML(String bucket, String key) {
+    UUID uuid = UUID.randomUUID();
+    final String source_idml_fn = "/tmp/" + uuid.toString() + ".idml";
 		try {
 			S3Object o = s3.getObject(bucket, key);
 			S3ObjectInputStream s3is = o.getObjectContent();
-      logger.info("Downloading IDML file from S3 (arn:aws:s3:::" + bucket + "/" + key + ") to " + input_fn);
-			FileOutputStream fos = new FileOutputStream(new File(input_fn));
+      logger.info("Downloading IDML file from S3 (arn:aws:s3:::" + bucket + "/" + key + ") to " + source_idml_fn);
+			FileOutputStream fos = new FileOutputStream(new File(source_idml_fn));
 			byte[] read_buf = new byte[1024];
 			int read_len = 0;
 			while ((read_len = s3is.read(read_buf)) > 0) {
@@ -82,11 +86,14 @@ public class IDMLHandler {
 			System.err.println(e.getMessage());
 			System.exit(1);
 		}
+    return source_idml_fn;
   }
 
-  private String uploadOutputXML(String output_bucket, String output_fn) {
+  private String uploadOutputXML(String output_bucket, String output_fn, String input_key) {
     UUID uuid = UUID.randomUUID();
-    final String output_key = uuid.toString() + ".xml";
+    final String basename = FilenameUtils.getBaseName(input_key);
+    final String output_key = basename + ".xml";
+
     final String arn = "arn:aws:s3:::" + output_bucket + "/" + output_key;
 
 		try {
