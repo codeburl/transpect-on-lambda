a546502255b96a65953c160bf0049afc4e8b7442
Keith Fahlgren <keith@codeburl.com>
2020-02-19 09:33:56 -0800

    Update deployment to reflect new SAM CLI interface

--- idml2xml-sam/deploy.sh
+++ idml2xml-sam/deploy.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 . .env
 
-dn=`dirname "$0"`
-profile=default
 bucket=${IDML2XML_SAM_PREFIX}-bucket
 
-#sam package \
-#  --profile ${AWS_PROFILE-$profile} \
-#  --s3-bucket ${bucket} \
-#  --output-template packaged.yml \
-#  --template-file template.yml
 sam deploy \
-  --profile ${AWS_PROFILE-$profile} \
-  --region us-east-1 \
   --capabilities CAPABILITY_IAM \
+  --profile $AWS_PROFILE \
+  --region $AWS_DEFAULT_REGION \
   --s3-bucket ${bucket} \
   --stack-name ${IDML2XML_SAM_PREFIX}-stack
